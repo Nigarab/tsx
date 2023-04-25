61d9516f430addd36a18b0f203507f5a9688044d
Kerim <asd@aasksd.com>
2023-04-25 18:10:49 +0400

    commit

---
+++ src/app/components/homepage.tsx
@@ -0,0 +1,135 @@
+<div className="freebie">
+    <div className="container">
+      <div className="background">
+        <div className="hero-title">
+          We build brands,<br />
+          relations & trust.
+        </div>
+        <div className="for-button">
+          <button className="for-container">Get started</button>
+        </div>
+      </div>
+     
+    </div>
+    <div className="about-container">
+     <div className="main-container">
+    <h1 className="about">Who we are</h1>
+    <div className="button-lorem-container">
+    <p className="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit vehicula <br/> dui sit amet ligula cursus. dolor sit amet dolor adipiscing.</p>
+  </div></div>
+    <div className="button-container"><button className="for-button1">Read more</button></div>
+  <div className="how-it-works">
+      <h1 className="how-container">Actually how it works</h1>
+    </div>
+  </div>
+  <div className="for-pic-and-write">
+        <div className="pic-side">
+          
+      <img src="./process 1 - Image.png" className="left"/></div>
+      <div className="write-side"><h2 className="right-write">Research prospect</h2>
+      <p className="right-write-lorem">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi illo distinctio</p>
+     <div className="button-left"><button className="how-button">Read more</button></div>
+    </div>
+  </div>
+
+    <div className="for-write-and-pic">
+      <div className="left-write"><h2>Deliver products</h2>
+    <p className="left-lorem">Lorem ipsum dolor, sit amet consectetur<br/> adipisicing elit. Expedita dolore aut </p>
+    <div className="button-right"><button className="how-button2">Read more</button></div> </div>
+    <div className="right-pic">
+      
+      <img src="./process 2 - Image.png" className="right"/>
+    </div>
+ 
+  </div>
+  <div className="specialized">
+    <h1 className="spe-container">We specialized in</h1>
+  </div>
+  <div className="grid2">
+    <div className="first-side-grid">
+    <div className="first-side">
+      <img src="./Icon1..png" className="icon1"/></div>
+      <h2 className="icon1-write">Marketing automation</h2>
+      <p className="icon1-p">Lorem ipsum dolor sit  amet,<br/> consectetur elie Duis sit.</p>
+      <div className="button1-side">
+      <button className="icon1-b">Read more</button></div>
+    </div>
+    
+    
+      <div className="second-side-grid">
+        <div className="second-side">
+      <img src="./Icon-2.png" className="icon2"/></div>
+      <h2 className="icon2-h1">Task management</h2>
+      <p className="icon2-p">Lorem ipsum dolor sit  amet,<br/> consectetur elie Duis sit.</p>
+    
+        <button className="icon2-b">Read more</button>
+      
+    </div>
+    <div className="first-side-grid">
+      <div className="first-side">
+        <img src="./Icon-3.png" className="icon1"/></div>
+        
+        
+        <h2 className="icon3-write1">Sales messaging</h2>
+        <p className="icon3-p1">Lorem ipsum dolor sit  amet,<br/> consectetur elie Duis sit.</p>
+        
+        <button className="icon1-b">Read more</button></div>
+      
+      <div className="second-side-grid">
+        <div className="second-side">
+      <img src="./Icon-4.png" className="icon2"/></div>
+      <h2 className="icon4-h1">Budgeting tools</h2>
+      <p className="icon4-p1">Lorem ipsum dolor sit  amet,<br/> consectetur elie Duis sit.</p>
+      
+        <button className="icon2-b">Read more</button>
+      
+    </div>
+  
+
+    </div>
+      
+
+  
+    <img src="./Single Image.png" className="like-video"/>
+    <h1 className="work">Work we have done</h1>
+    <div className="work-pictures">
+      <img src="./Image 1.png" className="image1"/>
+      <img src="./Image 2.png" className="image2"/>
+    </div>
+    <div className="work-pictures2">
+      <img src="./Image 3.png" className="image3"/>
+      <img src="./Image 4.png" className="image4"/>
+      <img src="./Image 5.png" className="image5"/>
+    </div>
+    <h1 className="what-we-post">What we post</h1>
+    <div className="what-container">
+      <div className="image6-first">
+      <img src="./Image 6.png" className="image6"/>
+      <h2 className="image6-h2">Improving performance<br/> of an online store</h2>
+      <p className="image6-p">Lorem ipsum dolor sit amet,<br/> consectetur elie Duis sit.</p>
+      <div className="image6-button">
+        <button className="button-left">Read more</button>
+      </div>
+    </div>
+    <div className="image6-second">
+      <img src="./Image 7.png" className="image7"/>
+      <h2 className="image6-h2">How to build and launch powerful websites</h2>
+      <p className="image6-p">Lorem ipsum dolor sit amet,<br/> consectetur elie Duis sit.</p>
+      <div className="image6-button">
+        <button className="button-left">Read more</button>
+    </div>
+  </div>
+    </div>
+    <div className="background-container">
+      <div className="background2">
+        <div className="hero-title-div">
+        <h1 className="hero-title2">Trust the experts for all your business needs</h1>
+      
+      </div>
+      <div className="back-button">
+      <button className="background-button2">Get started</button>
+    </div>
+    </div>
+
+    </div>
+    </div>
